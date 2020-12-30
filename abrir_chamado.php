3ca5506a14a940f06651f8e8f84cdba7e770e192
daviaquino87 <davi26031@gmail.com>
2020-12-30 12:40:53 -0300

    registrando chamados em arquivo txt

--- abrir_chamado.php
+++ abrir_chamado.php
@@ -43,15 +43,15 @@
               <div class="row">
                 <div class="col">
                   
-                  <form>
+                  <form method="POST" action="registra_de_chamado.php">
                     <div class="form-group">
                       <label>Título</label>
-                      <input type="text" class="form-control" placeholder="Título">
+                      <input name="titulo" type="text" class="form-control" placeholder="Título">
                     </div>
                     
                     <div class="form-group">
-                      <label>Categoria</label>
-                      <select class="form-control">
+                      <label >Categoria</label>
+                      <select name="categoria" class="form-control">
                         <option>Criação Usuário</option>
                         <option>Impressora</option>
                         <option>Hardware</option>
@@ -62,7 +62,7 @@
                     
                     <div class="form-group">
                       <label>Descrição</label>
-                      <textarea class="form-control" rows="3"></textarea>
+                      <textarea name="Descricao" class="form-control" rows="3"></textarea>
                     </div>
 
                     <div class="row mt-5">
---
+++ registra_de_chamado.php
@@ -0,0 +1,26 @@
+<?php
+//str_replace(); esta servido para caso seja enviado pelo formulario o nosso caractre separador '#' ...
+//ele fassa a mudança para o indicao '-';
+    $Titulo = str_replace('#' , '-' , $_POST['titulo'] );
+    $Categotia = str_replace('#' , '-' , $_POST['categoria'] );
+    $Descricao =  str_replace('#' , '-' , $_POST['Descricao'] );
+
+//PHP_EOL serve para quebrar a linha no arquivo txt;
+    $texto =  $Titulo . '#' . $Categotia . '#' . $Descricao . PHP_EOL;
+
+//fopen(); serve para criar/abrir um aquivo , o primeiro parametro serve para dizer o nome ...
+//do aquivo que sera criado , enquanto o segundo sera o metodo utilizado , que ...
+//no caso é 'a' que Abre somente para escrita; coloca o ponteiro do arquivo ...
+//no final do arquivo. Se o arquivo não existir, tenta criá-lo.;
+    $arquivo = fopen('dados.txt','a');
+
+// fwirete(); serve para escrever no arquivo , espera como ...
+//primeiro parametro nosso arquivo que foi criado e como segundo o que deve ser escrito nele;
+    fwrite($arquivo , $texto);
+
+//fclose(); serve para fechar o arquivo que foi criado e aguarda como parametro
+//o nome do aquivo;
+    fclose($arquivo);
+
+    header('location:abrir_chamado.php');
+?>
